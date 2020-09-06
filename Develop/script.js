74936c55978b9f521c0cc038286f1852b00e8f2a
astribli <andrewjstribling@gmail.com>
2020-09-06 02:35:22 -0500

    added color to times

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
 // Display current day and time
-var currentTime = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(currentTime)
+var date = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").text(date)
+
+// change color of textbox depending on time
+var present = moment();
+var future = moment().add(1, "hour");
+var past = moment().subtract(1, "hour");
+
+if (present) {
+    $("textarea").addClass("present");
+}
+else if (moment().isBefore(future)) {
+$("textarea").addClass("future");
+}
+
