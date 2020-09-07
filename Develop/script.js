075cb66acb57e8d0873473c0c96ffe1895467682
astribli <andrewjstribling@gmail.com>
2020-09-06 22:36:20 -0500

    storage function

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,8 @@
 var date = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(date)
 
+var tasks = {};
+
 // change color of textbox depending on time
 var present = moment();
 var future = moment().add(1, "hour");
@@ -11,6 +13,32 @@ if (present) {
     $("textarea").addClass("present");
 }
 else if (moment().isBefore(future)) {
-$("textarea").addClass("future");
+    $("textarea").addClass("future");
 }
 
+// Save tasks in local storage
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+};
+
+var auditTask = function () {
+    var time = moment().format("hA");
+    console.log(time);
+};
+
+// When save button is clicked
+$(".saveBtn").on("click", function () {
+    var text = $("textarea").text().trim();
+    console.log(text);
+
+    saveTasks();
+});
+
+
+setInterval(function () {
+    auditTask();
+}, (1000 * 60));
